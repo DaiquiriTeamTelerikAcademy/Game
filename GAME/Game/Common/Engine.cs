ed04a8b274205b0687d53427d6fa23f6ade2e765
EmiliyaDicheva <meminka_@hotmail.com>
2014-02-24 21:05:58 +0200

    Add class Engine

--- GAME/Game/Common/Engine.cs
+++ GAME/Game/Common/Engine.cs
@@ -18,5 +18,29 @@ namespace Game.Common
         {
             this.allObjects.Add(obj);
         }
+
+        public virtual void Run()
+        {
+            while (true)
+            {
+                this.renderer.RenderAll();//narisuvai kadar
+
+                System.Threading.Thread.Sleep(500);//iz4akai 0.5sec
+
+                this.userInterface.ProcessInput();//proverqva dali e natisnata nqkoq strelka , strelba i ako e si svar6i rabotata
+
+                this.renderer.ClearQueue();//za4isti bufera
+
+                foreach (var obj in this.allObjects)
+                {
+                    obj.Move();//pridviji obekta na kadeto e tragnal
+                    this.renderer.EnqueueForRendering(obj);//narisuvai obekta
+                }
+
+                //CollisionDispatcher.HandleCollisions(this.allObjects);
+
+                this.allObjects.RemoveAll(obj => obj.IsDestroyed);//iztriva obektite koito sa ubiti
+            }
+        }
     }
 }
