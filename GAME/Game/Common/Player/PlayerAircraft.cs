09161e984bbc665a331967ab87f9a1f74e4b5eea
EmiliyaDicheva <meminka_@hotmail.com>
2014-02-24 18:38:01 +0200

    Add class PlayerAircraft

--- GAME/Game/Common/Player/PlayerAircraft.cs
+++ GAME/Game/Common/Player/PlayerAircraft.cs
@@ -1,6 +1,24 @@
 ﻿namespace Game.Common.Player
 {
-    public class PlayerAircraft
+    public class PlayerAircraft : MovingObject
     {
+        public new const string CollisionGroupString = "aircraft";
+
+        public PlayerAircraft(MatrixCoords topLeft, MatrixCoords speed) : base(topLeft, new char[,] { { '^', '^', '^', '^', '^', '^', '^', '^', } }, speed) { }
+
+        public override bool CanCollideWith(string otherCollisionGroupString)
+        {
+            return otherCollisionGroupString == "big bomb" || otherCollisionGroupString == "small bomb";
+        }
+
+        public override string GetCollisionGroupString()
+        {
+            return PlayerAircraft.CollisionGroupString;
+        }
+
+        public override void RespondToCollision(CollisionData collisionData)
+        {
+            this.IsDestroyed = true;
+        }
     }
 }
